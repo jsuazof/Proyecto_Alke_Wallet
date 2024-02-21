c8bc6f03b36122bc2b359f75424efb5582db2339
Jorge Suazo <jsuazof@gmail.com>
2024-02-21 19:37:04 -0300

    add btn logout

--- js/script.js
+++ js/script.js
@@ -1,52 +1,57 @@
-$(document).ready(function() {
-    $('#loginForm').submit(function(event) {
-      event.preventDefault();
-      var username = $('#username').val();
-      var password = $('#password').val();
-  
-      // Verificar las credenciales
-      if (username === 'admin' && password === '12345') {
-        // Credenciales válidas, redirigir a la pantalla de wallet
-        window.location.href = '/html/menu.html';
-      } else {
-        // Credenciales inválidas, mostrar mensaje de error
-        alert('Usuario o contraseña invalido. Inténtalo de nuevo.');
-      }
-    });
-  });
-  
-  
-  
-  
-  $(document).ready(function() {
-    var balance = 0;
-  
-    function updateBalance() {
-      $('#balance').text(balance.toFixed(2));
+$(document).ready(function () {
+  $("#loginForm").submit(function (event) {
+    event.preventDefault();
+    var username = $("#username").val();
+    var password = $("#password").val();
+
+    // Verificar las credenciales
+    if (username === "admin" && password === "12345") {
+      // Credenciales válidas, redirigir a la pantalla de wallet
+      window.location.href = "/html/menu.html";
+    } else {
+      // Credenciales inválidas, mostrar mensaje de error
+      alert("Usuario o contraseña inválido. Inténtalo de nuevo.");
     }
-  
-    $('#depositBtn').click(function() {
-      var amount = parseFloat($('#amount').val());
-      if (!isNaN(amount) && amount > 0) {
-        balance += amount;
-        updateBalance();
-        $('#amount').val('');
-        alert('Deposit realizado!');
-      } else {
-        alert('Monto invalido. Por favor ingrese un número positivo.');
-      }
-    });
-  
-    $('#withdrawBtn').click(function() {
-      var amount = parseFloat($('#amount').val());
-      if (!isNaN(amount) && amount > 0 && amount <= balance) {
-        balance -= amount;
-        updateBalance();
-        $('#amount').val('');
-        alert('Retiro exitoso!');
-      } else {
-        alert('Cantidad no válida. Ingrese un número válido dentro de su saldo.');
-      }
+  });
+
+  // Corrección aquí: usar $(document).ready(function() {...})
+  $(document).ready(function () {
+    // Agrega un evento de clic al botón de logout
+    $("#logoutBtn").click(function () {
+      // Simulamos cerrar la sesión, puedes agregar tu lógica real aquí
+      alert("¡Has cerrado sesión!");
+      // Redirige a la página de inicio de sesión u otra página de tu elección
+      window.location.href = "/index.html";
     });
   });
-  
+
+  var balance = 0;
+
+  function updateBalance() {
+    $("#balance").text(balance.toFixed(2));
+  }
+
+  $("#depositBtn").click(function () {
+    var amount = parseFloat($("#amount").val());
+    if (!isNaN(amount) && amount > 0) {
+      balance += amount;
+      updateBalance();
+      $("#amount").val("");
+      alert("Depósito realizado!");
+    } else {
+      alert("Monto inválido. Por favor, ingrese un número positivo.");
+    }
+  });
+
+  $("#withdrawBtn").click(function () {
+    var amount = parseFloat($("#amount").val());
+    if (!isNaN(amount) && amount > 0 && amount <= balance) {
+      balance -= amount;
+      updateBalance();
+      $("#amount").val("");
+      alert("Retiro exitoso!");
+    } else {
+      alert("Cantidad no válida. Ingrese un número válido dentro de su saldo.");
+    }
+  });
+});
